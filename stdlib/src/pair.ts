78046350a563d8cef9319a012d298cfdfd97d334
Sean Brennan <whalesonstilts2012@gmail.com>
2024-08-05 17:06:08 -0700

    Updating projects (#7)
    y'know?

--- stdlib/src/pair.ts
+++ stdlib/src/pair.ts
@@ -4,25 +4,41 @@ import { isArray } from './array';
  * Types
  */
 
-export type Pair<TKey, TValue> = [TKey, TValue];
+export type Pair<TFirst, TSecond> = [TFirst, TSecond];
 
 /**
  * Initializers
  */
 
-export const Pair = <TKey, TValue>(key: TKey, value: TValue): Pair<TKey, TValue> => [key, value];
+export const Pair = <TFirst, TSecond>(first: TFirst, second: TSecond): Pair<TFirst, TSecond> => [first, second];
 
 /**
  * Functions
  */
 
-export const key = <TKey, TValue>(pair: Pair<TKey, TValue>): TKey => pair[0];
+export const first = <TFirst, TSecond>(pair: Pair<TFirst, TSecond>): TFirst => pair[0];
 
-export const value = <TKey, TValue>(pair: Pair<TKey, TValue>): TValue => pair[1];
+export const mapFirst = <TFirstX, TFirstY, TSecond>(
+    pair: Pair<TFirstX, TSecond>,
+    map: (first: TFirstX) => TFirstY,
+): Pair<TFirstY, TSecond> => Pair(map(first(pair)), second(pair));
+
+export const mapPair = <TFirstX, TSecondX, TFirstY, TSecondY>(
+    pair: Pair<TFirstX, TSecondX>,
+    mapFirst: (first: TFirstX) => TFirstY,
+    mapSecond: (second: TSecondX) => TSecondY,
+): Pair<TFirstY, TSecondY> => Pair(mapFirst(first(pair)), mapSecond(second(pair)));
+
+export const mapSecond = <TFirst, TSecondX, TSecondY>(
+    pair: Pair<TFirst, TSecondX>,
+    map: (first: TSecondX) => TSecondY,
+): Pair<TFirst, TSecondY> => Pair(first(pair), map(second(pair)));
+
+export const second = <TFirst, TSecond>(pair: Pair<TFirst, TSecond>): TSecond => pair[1];
 
 /**
  * Type guards
  */
 
-export const isPair = <TKey, TValue>(value: unknown): value is Pair<TKey, TValue> =>
+export const isPair = <TFirst, TSecond>(value: unknown): value is Pair<TFirst, TSecond> =>
     isArray(value) && value.length === 2;
