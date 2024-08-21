c79d87c7544830bb1006a3339a00bb88c78507b0
Sean Brennan <whalesonstilts2012@gmail.com>
2024-08-20 17:57:50 -0700

    reporters

--- jest.config.ts
+++ jest.config.ts
@@ -3,4 +3,5 @@ import { getJestProjects } from '@nx/jest';
 export default {
     codeCoverage: true,
     projects: getJestProjects(),
+    reporters: [['github-actions', { silent: false }], 'summary'],
 };
